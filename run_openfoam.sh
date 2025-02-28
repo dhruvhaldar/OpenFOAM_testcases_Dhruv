d3ed99149e009d29ff212219247366b5adab5106
Dhruv Haldar <dhruv@sankhyasutralabs.com>
2025-02-28 23:50:35 +0530

    update run_openfoam.sh

--- run_openfoam.sh
+++ run_openfoam.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -ti -v $(pwd):/root/OpenFOAM/user-v2412/work ubuntu_bionic_openfoam2412
+docker run -ti -v $(pwd):/root/OpenFOAM/user-v2412/work haldardhruv/ubuntu_bionic_openfoam2412-dhruv
