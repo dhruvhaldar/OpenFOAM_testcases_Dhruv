ab70a99997ff372f406f3d8d53dd08e904b46090
Dhruv Haldar <dhruv@sankhyasutralabs.com>
2025-03-01 00:23:08 +0530

    update run_openfoam.sh

--- run_openfoam.sh
+++ run_openfoam.sh
@@ -4,6 +4,10 @@
 if [ "$SHELL" = "/usr/bin/fish" ] || [ "$SHELL" = "/bin/fish" ]; then
     # Execute the Fish command using Fish shell
     fish -c 'docker run -ti -v (pwd):/root/OpenFOAM/user-v2412/work haldardhruv/ubuntu_bionic_openfoam2412-dhruv'
+# Check if the shell is Zsh
+elif [ "$SHELL" = "/usr/bin/zsh" ] || [ "$SHELL" = "/bin/zsh" ]; then
+    # Execute the command using Zsh syntax (same as Bash in this case)
+    docker run -ti -v $(pwd):/root/OpenFOAM/user-v2412/work haldardhruv/ubuntu_bionic_openfoam2412-dhruv
 else
     # Default to Bash syntax
     docker run -ti -v $(pwd):/root/OpenFOAM/user-v2412/work haldardhruv/ubuntu_bionic_openfoam2412-dhruv
