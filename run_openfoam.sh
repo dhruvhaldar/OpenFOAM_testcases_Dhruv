ef8bb01bbb99743bd478e517bb33028a55759ac1
Dhruv Haldar <dhruv@sankhyasutralabs.com>
2025-03-01 00:07:25 +0530

    update run_openfoam.sh

--- run_openfoam.sh
+++ run_openfoam.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run -ti -v $(pwd):/root/OpenFOAM/user-v2412/work haldardhruv/ubuntu_bionic_openfoam2412-dhruv
+# Check if the shell is Fish
+if [ "$SHELL" = "/usr/bin/fish" ] || [ "$SHELL" = "/bin/fish" ]; then
+    # Fish shell syntax
+    docker run -ti -v (pwd):/root/OpenFOAM/user-v2412/work haldardhruv/ubuntu_bionic_openfoam2412-dhruv
+else
+    # Default to Bash syntax
+    docker run -ti -v $(pwd):/root/OpenFOAM/user-v2412/work haldardhruv/ubuntu_bionic_openfoam2412-dhruv
+fi
